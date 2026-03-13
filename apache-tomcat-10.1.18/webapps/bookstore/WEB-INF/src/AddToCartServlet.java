32b10456d5cb82406a0d5366a831e06872c0ef38
soumyakhore-cyber <soumyakhore@gmail.com>
2026-03-13 11:21:06 +0000

    experiment 2 done

---
+++ apache-tomcat-10.1.18/webapps/bookstore/WEB-INF/src/AddToCartServlet.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.util.*;
+
+import jakarta.servlet.http.HttpServlet; // IT IS A BASE CLASS FOR CREATING THE HTTP SERVLET
+import jakarta.servlet.http.HttpServletRequest;  // IT IS USED TO GET THE DATA FROM THE CLINT
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class AddToCartServlet extends HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException,IOException{
+        String book = request.getParameter("book");
+
+        HttpSession session = request.getSession();
+
+        List<String> cart = (List<String>) session.getAttribute("cart");
+
+        if(cart == null){
+            cart = new ArrayList<>();
+        }
+
+        cart.add(book);
+
+        session.setAttribute("cart",cart);
+
+        response.sendRedirect("index.jsp");
+    }
+}
---
+++ apache-tomcat-10.1.18/webapps/bookstore/WEB-INF/src/ViewCartServlet.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.util.*;
+import jakarta.servlet.http.HttpServlet; // IT IS A BASE CLASS FOR CREATING THE HTTP SERVLET
+import jakarta.servlet.http.HttpServletRequest;  // IT IS USED TO GET THE DATA FROM THE CLINT
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class ViewCartServlet extends HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException,IOException{
+        HttpSession session = request.getSession();
+
+        List<String> cart = (List<String>) session.getAttribute("cart");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<h2>Your Cart</h2>");
+
+        if(cart != null){
+            for(String book : cart){
+                out.println(book + "<br>");
+            }
+        }else{
+            out.println("Cart is empty");
+        }
+    }
+}
